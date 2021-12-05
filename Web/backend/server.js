c03e7fac83346c3f9abaf7cef8fb2123b5e4f3cb
fishalx6263@gmail.com <fishalx6263@gmail.com>
2021-12-05 15:07:23 +0200

    server

--- Web/backend/server.js
+++ Web/backend/server.js
@@ -40,6 +40,10 @@ app.use('/api/blogs', blogRoutes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      });
 }
 
 app.listen(PORT, console.log('Server starting at '+PORT));
