9dcab85534d94e1da68eaf340b0e73d0e4eb2b58
quepena <fishalx6263@gmail.com>
2022-12-26 14:01:30 +0200

    deploy

---
+++ Web/frontend/build/precache-manifest.72fc980685c457453c51b0ac4b890f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3b1466fcfed2257adbff53de8bdfb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7e03e3d7aa43ab9cf40",
+    "url": "/static/css/main.bb054687.chunk.css"
+  },
+  {
+    "revision": "d236870b1df99afaf417",
+    "url": "/static/js/2.3bbf8d8a.chunk.js"
+  },
+  {
+    "revision": "c913d2500e9211e9d0aa80f327b9cd6f",
+    "url": "/static/js/2.3bbf8d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773dbe27e144bf567ea3",
+    "url": "/static/js/3.b76168e5.chunk.js"
+  },
+  {
+    "revision": "e7e03e3d7aa43ab9cf40",
+    "url": "/static/js/main.3f3ebc11.chunk.js"
+  },
+  {
+    "revision": "bbdbfece3c9b6b1626aa",
+    "url": "/static/js/runtime-main.7b2ed3f3.js"
+  }
+]);
